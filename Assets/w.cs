09ba5ccd253ffba5dfc499c3a309449748069c40
Atlas5648k <nitteibannzai@gmail.com>
2021-08-18 13:05:27 +0900

    キューブとスクリプトを追加

---
+++ Assets/w.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class w : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("wwww");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
