800d93e362f8395fc181ccf759be3d8e75a87bb5
Atlas5648k <nitteibannzai@gmail.com>
2021-08-18 13:19:24 +0900

    スクリプトを変更しました

--- Assets/w.cs
+++ Assets/w.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class w : MonoBehaviour
 {
+    [SerializeField] float Animeter;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class w : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("草"); 
     }
 }
